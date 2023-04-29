c875ea630b307499b7b735d748aa30d355d9c40c
petekostrytsya <petekostrytsya@gmail.com>
2023-04-29 14:52:37 +0300

    addProject

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// const title = document.querySelector(".cats-title");
+
+// const logotype = document.querySelector(".site-logo img");
+// const body = document.querySelector("body");
+// console.log(logotype);
+
+// function onLogoClick() {
+//   body.style.backgroundColor = "teal";
+// }
+
+// title.addEventListener("click", onLogoClick);
+
+const slider = document.querySelector(".hero-list");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+const mySiema = new Siema({
+  selector: slider,
+  loop: true,
+  duration: 500
+});
+
+function onNextClick() {
+  mySiema.next();
+}
+
+function onPrevClick() {
+  mySiema.prev();
+}
+
+next.addEventListener("click", onNextClick);
+prev.addEventListener("click", onPrevClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+});
+
+AOS.init();
